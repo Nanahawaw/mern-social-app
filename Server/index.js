7ff1a18e6be1ba1f63fd227b9d4ae74a04771d30
Nanahawaw <folarinyetunde@gmail.com>
2023-10-11 20:28:01 +0100

    creating routes using express router system

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+//database
 const db = require("./models");
 
+//routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+//api request
 db.sequelize.sync().then(() => {
   app.listen(5000, () => {
     console.log("Hello World");
